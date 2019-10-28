c2be9aa2632ab4e5d82ef0360c529e77990fd240
Simonster04 <simon.pahe@gmail.com>
2019-10-28 03:03:35 +0000

    Segundo día

---
+++ operaciones.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <math.h>
+
+int main ()
+{
+	int x, y;
+
+	x = 10;
+	y = 2;
+
+	/*Operaciones básicas*/
+
+	x += y;
+	printf("El valor de x + y es: %i\n", x);
+
+	x -= y;
+	printf("El valor de x - y es: %i\n", x);
+
+	x *= y;
+	printf("El valor de x * y es: %i\n", x);
+
+	x /= y;
+	printf("El valor de x / y es: %i\n", x);
+
+	x = x % y;
+	printf("El valor de x resto y es: %i\n", x);
+
+	x = 10;
+
+	/*Operaciones con math.h*/
+	x = pow (y, x);
+        printf("El valor de y elevado a la x es: %i\n", x);
+
+	x = sqrt (x);
+        printf("El valor de raiz cuadrada de x es: %i\n", x);
+
+	/*Operadores de incremento*/
+
+	x++;
+	printf("El valor de x++ es: %i\n", x);
+
+	x--;
+	printf("El valor de x-- es: %i\n", x);
+
+	return (0);
+}
